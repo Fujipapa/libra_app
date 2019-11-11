797971c6f4d9b4b496016093f63a153591bafeac
Reo Fujii <fujipapadesu@gmail.com>
2019-11-11 17:50:46 +0900

    Add validates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,28 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(email: "Example@example.com", password: "Example", role: 1)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "password shoule be present" do
+    @user.password = ""
+    assert_not @user.valid?
+  end
+
+  test "role should be present" do
+    @user.password = nil 
+    assert_not @user.valid?
+  end
+  
 end
