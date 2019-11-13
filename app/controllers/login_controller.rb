3c3cd9f953ec99dcb721f72d1dff1b5c6b1e0fb4
Reo Fujii <fujipapadesu@gmail.com>
2019-11-13 19:13:57 +0900

    feature login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,6 +8,7 @@ class LoginController < ApplicationController
   def create
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       redirect_to user_url(@user.id)
     else
       @password = params[:password]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
                         password: params[:password], 
                         role: 1)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_url(@user.id)
         else
             render('users/new')
