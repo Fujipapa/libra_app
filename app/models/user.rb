ae6a19bb0ff8ce79600ee3118f47b8a97eddf788
Reo Fujii <fujipapadesu@gmail.com>
2019-11-27 20:40:58 +0900

    finish login test

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,5 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
-      end
+    end
 end
