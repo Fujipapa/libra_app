917f5d35954ec9769e0e9c6213a74d6cd859c0b8
Reo Fujii <fujipapadesu@gmail.com>
2019-11-27 16:14:47 +0900

    finish feature-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqueness: true
+    before_save {email.downcase!}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                        uniqueness: {case_sensitive: false}, 
+                        length: {maximum: 255},
+                        format: {with: VALID_EMAIL_REGEX}
+
     validates :role, presence: true
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
