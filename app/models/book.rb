e16f92aabbe02cf4621be1d9a05bfd2f63576c70
Reo Fujii <fujipapadesu@gmail.com>
2019-11-29 05:15:28 +0900

    Add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true, length: {maximum: 255}
     validates :title_kana, presence: true, length: {maximum:255}
+    validates :genre_id, presence: true
     enum status: {可: true, 不可: false}
 end
