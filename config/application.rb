6f7c101b742a2eb809ed8927a67438f7c3274c11
Reo Fujii <fujipapadesu@gmail.com>
2019-12-11 02:09:04 +0900

    Edit puma setting

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module LibraApp
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
