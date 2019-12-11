c89924232e55a8a6175f4b56f0ad4b860e52b0fe
Reo Fujii <fujipapadesu@gmail.com>
2019-12-11 11:49:20 +0900

    Edit chenge_column_to_users_role migration

--- db/migrate/20191204071136_change_column_to_users_role.rb
+++ db/migrate/20191204071136_change_column_to_users_role.rb
@@ -1,6 +1,7 @@
 class ChangeColumnToUsersRole < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :role, 'USING status::integer', default: 0
+    remove_column :users, :role, :integer
+    add_column :users, :role, :integer, default: 0
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
