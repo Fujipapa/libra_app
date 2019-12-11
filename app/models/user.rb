bef2090859a08884a2588d4d432379e71ef10ebf
Reo Fujii <fujipapadesu@gmail.com>
2019-12-11 11:02:15 +0900

    deleto role validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
                         length: {maximum: 255},
                         format: {with: VALID_EMAIL_REGEX}
 
-    validates :role, presence: true
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
